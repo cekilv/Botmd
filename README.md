8ce56140a47ea6e760d2081367da0b2c0e1aceb6
cekil23 <cekilcekil40@gmail.com>
2022-07-01 06:41:00 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+<div align="center">
+<img src="https://i.ibb.co/d2hqN4D/051f190bb343.jpg" alt="Sebut Saja Jaki" width="300" />
+
+Jika Dirimu Pernah Menyesalinya Lantas Kenapa Kamu Mengulanginnya
+
+>
+>
+>
+</div>
+<p align="center">
+  <a href="https://github.com/KiZakiXD"><img title="Si Paling" src="https://img.shields.io/badge/Author-KiZakiXD-red.svg?style=for-the-badge&logo=github" /></a>
+  <h4 align="center">
+  <a href="https://wa.me/6285878313791"> No Enc? </a>
+</h4>
+</p>
+
+- 🌱 I Can Codes In :
+
+  - Javascript
+
+  - HTML
+
+  - CSS
+
+ ✨ My Waifu :
+   
+  - Chitanda Eru    
+
+
+**Information:**
+
+- [My Chanel Youtube](https://m.youtube.com/channel/UCHdipvk52k43KsPRenGpD0w)
+
+- [Donate](wa.me/6285878313791)
+
+## <div align="center">⚡ Information </div>
+
+<div align="center">
+
+<img alt="JavaScript" src="https://img.shields.io/badge/javascript%20-%23323330.svg?&style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"/>
+
+<img alt="HTML" src="https://img.shields.io/badge/html5%20-%23E34F26.svg?&style=for-the-badge&logo=html5&logoColor=white"/>
+
+<img alt="CSS" src="https://img.shields.io/badge/css3%20-%231572B6.svg?&style=for-the-badge&logo=css3&logoColor=white"/>
+
+<img alt="Express.js" src="https://img.shields.io/badge/express.js%20-%23404d59.svg?&style=for-the-badge"/>
+
+<img alt="Git" src="https://img.shields.io/badge/git%20-%23F05033.svg?&style=for-the-badge&logo=git&logoColor=white"/>
+
+<img alt="MongoDB" src ="https://img.shields.io/badge/MongoDB-%234ea94b.svg?&style=for-the-badge&logo=mongodb&logoColor=white"/>
+
+<img alt="Bootstrap" src="https://img.shields.io/badge/bootstrap%20-%23563D7C.svg?&style=for-the-badge&logo=bootstrap&logoColor=white"/>
+
+</div>
+
+___
+
+![Zhirrr github stats](https://github-readme-stats.vercel.app/api?username=KiZakiXD&show_icons=true&theme=tokyonight)
+
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=KiZakiXD&hide=css,html&theme=tokyonight)
+
+## CARA INSTALL CEK DI FILE
+
+---------
+
+
+  # Thanks To
+* [`ALL CREATOR`]
---
+++ config.js
@@ -0,0 +1,407 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.thum = fs.readFileSync("./media/zaki.jpg")
+
+global.autoReadAll = true 
+
+// '6285878313791' ganti nomor kalian '628xxxxxx'
+//KiZakiXD ganti nama kalian xxxxz
+//Silahkan sesuaikan sendiri ya
+
+// Ganti Sewajarnya
+global.owner = ['6289515023118']
+global.pengguna = 'CekilXD'
+global.premium = []
+global.author = 'CekilXD'
+global.pemilik = ['6289515023118'] 
+global.ownernomer = '6289515023118'
+global.ownername = 'cekilXd'
+global.botname = 'CekilBOT - MD'
+global.tengah = "									"
+global.Prefix = 'Multi Prefix'
+global.meki = '6289515023118'
+global.footer = 'CEKIL - MD'
+//Isi Sesuai Namalu Buat Tambah Tqtq
+global.namalu = 'CekilXd' //Ubah nama kalian buat nambahin di tqtq bot
+global.footer1 = 'Note : Jika Kamu Menggunakan Whatshapp Mod Atau Versi Lama Button Menu Tidak Kelihatan Silahkan Ketik allmenu Untuk Fitur Bot'
+global.footer2 = 'Jika Menemukan Bug Lapor Ke Owner.'
+global.yt = 'https://cekilgans.github.io' //Jika tidak ada yt jangan diubah buat promosi owner xixixixi
+global.ig = 'https://instagram.com/cekilgans'
+global.gc = 'https://chat.whatsapp.com/CF4jm8T9FVx5MnD037B1nN' //join ya kadang kadang pada eval bot disitu lumayan dapet fitur baru wkwkwk
+global.dana = '089515023118'
+global.gopay = '089515023118'
+global.wame = 'wa.me/6289515023118'
+global.email = 'cekilxd@gmail.com'
+global.region = 'Indonesia'
+global.link = 'https://chat.whatsapp.com/CF4jm8T9FVx5MnD037B1nN/'
+global.sc = 'https://cekilgans.github.io'
+global.packname = '"{\nAuthor: CEKIL"\n"Bot Number: 6289515023118"\n"Bot Name: Cekil - MD"\n"Instagram: @cekilgans"\n\n}' 
+global.author = '' 
+global.sessionName = 'session'
+global.prefa = ['#','!','/','']
+global.sp = '❑ ' 
+global.mess = {
+    success: 'Apaan Banh?',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    error: 'Fitur sedang error!',
+        endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+    wrongFormat: 'Perintah Salah!!\nSertakan Link setelah Command..',
+    example1: 'Welcome @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @subject (Group Name)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)'
+,
+    example2: 'Good Bye @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @subject (Group Name)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)'    
+    
+}
+    global.limitawal = {
+    premium: "Unlimited",
+    free: 30,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./media/zaki.jpg')
+global.fakeImg = fs.readFileSync('./media/fake.jpg')
+
+//Ambil dr chika
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//Menu
+global.ccomannya = `
+╭───l「 *Main Menu* 」
+├ script
+├ speedtest
+├ ping
+├ owner
+├ menu
+├ delete
+├ infochat
+├ quoted
+├ listpc
+├ listonline
+├  
+├───l「 *Group Menu* 」
+├ linkgroup
+├ ephemeral [option]
+├ setppgc [image]
+├ setname [text]
+├ setdesc [text]
+├ group [option]
+├ editinfo [option]
+├ add @user
+├ kick @user
+├ hidetag [text]
+├ tagall [text]
+├ antilink [on/off]
+├ mute [on/off]
+├ promote @user
+├ demote @user
+├ vote [text]
+├ devote
+├ upvote
+├ cekvote
+├ hapusvote
+├
+├───l 「 *Owner Menu* 」
+├ react [emoji]
+├ chat [option]
+├ join [link]
+├ leave
+├ block @user
+├ setbiobot [text] 
+├ unblock @user
+├ bcgroup [text]
+├ bcall [text]
+├ setppbot [image]
+├ setexif
+├  
+├───l「 *Rpg Menu* 」
+├ berburu
+├ mancing
+├ menambang
+├ mining
+├ heal
+├ profile
+├ inventory
+├ leaderboard
+├ buy
+├ sell
+├
+├───l「 *Downloader Menu* 」
+├ tiktok [url]
+├ instagram [url]
+├ twitter [url]
+├ facebook [url]
+├ igstory [query]
+├ umma [url]
+├ mediafire [url]
+├ gitclone [url]
+├ 
+├───l「 *Text Pro Menu* 」
+├ textmaker glicth [Text]
+├ textmaker glow [Text]
+├ flasmurf [Text]
+├ flarun [Text]
+├ fluming [Text]
+├ flaming [Text]
+├ lava [Text]
+├ neonlight [Text]
+├ fabric [Text]
+├ glue [Text]
+├ luxury [Text]
+├ underwater [Text]
+├ harrypotter [Text]
+├ steel [Text]
+├ metallic [Text]
+├ graffiti [Text]
+├ pencil [Text]
+├ sand [Text]
+├ magma [Text]
+├ joker [Text]
+├ sky [Text]
+├ matrix [Text]
+├ neon [Text]
+├ blackpink [Text]
+├ thunder [Text]
+├ whitebear [Text]
+├ hoorror [Text]
+├ 8bit [Text]
+├ horror [Text]
+├ retro [Text]
+├ pornhub [Text]
+├ juice [Text]
+├ batman [Text]
+├ multicolor [Text]
+├ collwall [Text]
+├ cool [Text]
+├ wonderful [Text]
+├ sketch [Text]
+├ skeleton [Text]
+├ marvel [Text]
+├ foggy [Text]
+├ writing [Text]
+├ halloween [Text]
+├ watercolor [Text]
+├ classic [Text]
+├ rainbow [Text]
+├ sci_fi [Text]
+├ christmas [Text]
+├ ancient [Text]
+├ toxic [Text]
+├
+├───l「 *Stalker Menu* 」
+├ ghstalk [query]
+├ igstalk [query]
+├ ffstalk [query]
+├ supersusstalk [query]
+├
+├───l「 *Internet Menu* 」
+├ ip [query]
+├ iploc [query]
+├ ipinfo [query]
+├ ssweb [query]
+├  
+├───l「 *Search Menu* 」
+├ play [query]
+├ yts [query]
+├ google [query]
+├ gimage [query]
+├ pinterest [query]
+├ wallpaper [query]
+├ wikimedia [query]
+├ ytsearch [query]
+├ ringtone [query]
+├ stalk [option] [query]
+├ webtoons [query]
+├ drakor [query]
+├ anime [query]
+├ character [query]
+├ manga [query]
+├
+├───l「 *Random Menu* 」
+├ coffe
+├ quotesanime
+├ couple
+├ anime
+├ waifu (nsfw)
+├ husbu (nsfw)
+├ neko (nsfw)
+├ shinobu (nsfw)
+├ waifus (nsfw)
+├ nekos (nsfw)
+├ trap (nsfw)
+├ blowjob (nsfw)
+├
+├───l「 *Random Anime Menu* 」
+├ loli
+├ neko
+├ waifu
+├ shinobu
+├ megumin
+├ bully
+├ cuddle
+├ cry
+├ hug
+├ awoo
+├ kiss
+├ lick
+├ pat
+├ smug
+├ bonk
+├ yeet
+├ blush
+├ smile
+├ wave
+├ highfive
+├ handhold
+├ nom
+├ bite
+├ glomp
+├ slap
+├ kill
+├ happy
+├ wink
+├ poke
+├ dance
+├ cringe
+├
+├───l「 *Fun Menu* 」
+├ bagaimanakah
+├ kapankah
+├ apakah
+├ bisakah
+├ rate
+├ wangy
+├ gantengcek
+├ cekganteng
+├ cantikcek
+├ cekcantik
+├ sangecek
+├ ceksange
+├ cekme
+├ gaycek
+├ cekgay
+├ lesbicek
+├ halah
+├ hilih
+├ huluh
+├ heleh
+├ holoh
+├ jadian
+├ jodohku
+├ delttt
+├ tictactoe
+├ family100
+├ tebak [option]
+├ math [mode]
+├ suitpvp [@tag]
+├
+├───l「 *Primbon Menu* 」
+├ nomorhoki
+├ artimimpi
+├ artinama
+├ ramaljodoh
+├ ramaljodohbali
+├ suamiistri
+├ ramalcinta
+├ cocoknama
+├ pasangan
+├ jadiannikah
+├ sifatusaha
+├ rezeki
+├ pekerjaan
+├ nasib
+├ penyakit
+├ tarot
+├ fengshui
+├ haribaik
+├ harisangar
+├ harisial
+├ nagahari
+├ arahrezeki
+├ peruntungan
+├ weton
+├ karakter
+├ keberuntungan
+├ memancing
+├ masasubur
+├ zodiak
+├ shio
+├
+├───l「 *Convert Menu* 」
+├ toimage
+├ removebg
+├ sticker
+├ emojimix
+├ tovideo
+├ togif
+├ tourl
+├ tovn
+├ tomp3
+├ toaudio
+├ ebinary
+├ dbinary
+├ styletext
+├ smeme
+├
+├───l「 *Database Menu* 」
+├ setcmd
+├ listcmd
+├ delcmd
+├ lockcmd
+├ addmsg
+├ listmsg
+├ getmsg
+├ delmsg
+├
+├───l「 *Anonymous Menu* 」
+├ anonymous
+├ start
+├ next
+├ keluar
+├ sendkontak
+├
+├───l「 *Islamic Menu* 」
+├ iqra
+├ hadist
+├ alquran
+├ juzamma
+├ tafsirsurah
+├
+├───l「 *Voice Changer* 」
+├ bass
+├ blown
+├ deep
+├ earrape
+├ fast
+├ fat
+├ nightcore
+├ reverse
+├ robot
+├ slow
+╰───l tupai
+`
+global.sewanya =
+'  *» List Sewabot :*\n   _• Sewa Bot 1 Minggu : 7K_\n   _• Sewa Bot 1 Bulan : 15K_\n   _• Sewa Bot 1 Tahun : 30K_'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
